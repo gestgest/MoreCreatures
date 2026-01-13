79f21ad23415bd235ef51c8ea76f7c32d3d0f6af
gestgest <87427692+gestgest@users.noreply.github.com>
2026-01-14 04:41:39 +0900

    add ground 2

---
+++ MoreCreatures/src/header/GameObject/GameObject.h
@@ -0,0 +1,338 @@
+#ifndef GAMEOBJECT
+#define GAMEOBJECT
+#include <header/shader.h>
+#include <header/camera.h>
+#include <glad/glad.h>
+
+// settings
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 800;
+
+#define COR  -0.33f; //반발계수
+#define GRAVITY_ACCELERATION -9.81f
+
+void debugMat(glm::mat4 m)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout << m[i][j] << ' ';
+        }
+        std::cout << '\n';
+
+    }
+
+}
+
+bool isInBoundary(float a, float b, float a_size, float b_size)
+{
+    float a_1 = a - a_size / 2;
+    float a_2 = a + a_size / 2;
+    float b_1 = b - b_size / 2;
+    float b_2 = b + b_size / 2;
+
+    if
+        (
+            (a_1 <= b_1 && b_1 <= a_2) ||
+            (a_1 <= b_2 && b_2 <= a_2)
+            )
+    {
+        return true;
+    }
+    else if
+        (
+            (b_1 <= a_1 && a_1 <= b_2) ||
+            (b_1 <= a_2 && a_2 <= b_2)
+            )
+    {
+        return true;
+    }
+    return false;
+}
+
+class GameObject {
+protected:
+    unsigned int vao;
+    unsigned int vbo;
+    Shader* shader;
+
+    int nSphereVert;
+    int nSphereAttr;
+
+    glm::vec3 scale; //물리 scale
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec3 color;
+
+    float movement_speed;
+
+    bool isStatic;
+    bool isActive = true;
+
+
+    void move(glm::vec3 velocity, float deltaTime)
+    {
+        position += velocity * deltaTime;
+    }
+
+
+    // position, normal, tex_coords.
+    // 매개변수 방정식으로 원 그리는 함수 => 
+    void init_sphere(float** vertices) //높이는 3.14
+    {
+        //nAttr : 8
+        // sphere: set up vertex data and configure vertex attributes
+        float pi = acosf(-1.0f);	// pi = 3.14152...
+        float pi2 = 2.0f * pi;
+        int nu = 40, nv = 20;
+        const double du = pi2 / nu;
+        const double dv = pi / nv;
+
+        //19 * 40 * 6
+        nSphereVert = (nv - 1) * nu * 6;		// two triangles
+        nSphereAttr = 6;
+        *vertices = (float*)malloc(sizeof(float) * (nSphereVert) * (nSphereAttr));
+
+        float u, v;
+        int k = 0;
+
+        v = 0.0f;
+        u = 0.0f;
+        for (v = (-0.5f) * pi + dv; v < 0.5f * pi - dv; v += dv)
+        {
+            for (u = 0.0f; u < pi2; u += du)
+            {
+                // p(u,v)
+                (*vertices)[k++] = cosf(v) * cosf(u); 	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v); 	// position (x,y,z)
+                (*vertices)[k++] = cosf(v) * cosf(u);	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v);		// normal (x,y z)
+                // 이렇게 해서 8개의 속성 => nAttr
+
+
+                // p(u+du,v)
+                (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v); 	// position
+                (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v);		// normal
+
+                // p(u,v+dv)
+                (*vertices)[k++] = cosf(v + dv) * cosf(u);	(*vertices)[k++] = cosf(v + dv) * sinf(u);	(*vertices)[k++] = sinf(v + dv);	// position
+                (*vertices)[k++] = cosf(v + dv) * cosf(u);	(*vertices)[k++] = cosf(v + dv) * sinf(u);	(*vertices)[k++] = sinf(v + dv);	// normal
+
+                // p(u+du,v)
+                (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v); 	// position
+                (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v);		// normal
+
+                // p(u+du,v+dv)
+                (*vertices)[k++] = cosf(v + dv) * cosf(u + du);	(*vertices)[k++] = cosf(v + dv) * sinf(u + du);	(*vertices)[k++] = sinf(v + dv); 	// position
+                (*vertices)[k++] = cosf(v + dv) * cosf(u + du);	(*vertices)[k++] = cosf(v + dv) * sinf(u + du);	(*vertices)[k++] = sinf(v + dv);	// normal
+
+                // p(u,v+dv)
+                (*vertices)[k++] = cosf(v + dv) * cosf(u);	(*vertices)[k++] = cosf(v + dv) * sinf(u);	(*vertices)[k++] = sinf(v + dv);	// position
+                (*vertices)[k++] = cosf(v + dv) * cosf(u);	(*vertices)[k++] = cosf(v + dv) * sinf(u);	(*vertices)[k++] = sinf(v + dv);	// normal
+            }
+        }
+        // triangles around north pole and south pole
+        for (u = 0.0f; u < pi2; u += du)
+        {
+            // triangles around north pole
+            // p(u,pi/2-dv)
+            v = 0.5f * pi - dv;
+            (*vertices)[k++] = cosf(v) * cosf(u); 	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v); 	// position
+            (*vertices)[k++] = cosf(v) * cosf(u);	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v);		// normal
+
+            // p(u+du,pi/2-dv)
+            v = 0.5f * pi - dv;
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du); (*vertices)[k++] = sinf(v); 	// position
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du); (*vertices)[k++] = sinf(v);		// normal
+
+            // p(u,pi/2) = (0, 1. 0)  ~ north pole
+            v = 0.5f * pi;
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v); 	 // position
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du);	(*vertices)[k++] = sinf(v);		 // normal
+
+            // triangles around south pole
+            // p(u,-pi/2) = (0, -1, 0)  ~ south pole
+            v = (-0.5f) * pi;
+            (*vertices)[k++] = cosf(v) * cosf(u); 	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v); 		// position
+            (*vertices)[k++] = cosf(v) * cosf(u);	(*vertices)[k++] = cosf(v) * sinf(u);	(*vertices)[k++] = sinf(v);			// normal
+
+            // p(u+du,-pi/2+dv)
+            v = (-0.5f) * pi + dv;
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du); (*vertices)[k++] = sinf(v);	// position
+            (*vertices)[k++] = cosf(v) * cosf(u + du);	(*vertices)[k++] = cosf(v) * sinf(u + du); (*vertices)[k++] = sinf(v);	// normal
+
+            // p(u,-pi/2+dv)
+            (*vertices)[k++] = cosf(v) * cosf(u);	(*vertices)[k++] = cosf(v) * sinf(u); (*vertices)[k++] = sinf(v);	// position
+            (*vertices)[k++] = cosf(v) * cosf(u);	(*vertices)[k++] = cosf(v) * sinf(u); (*vertices)[k++] = sinf(v);	// normal
+        }
+    }
+
+public:
+    GameObject()
+    {
+        scale = glm::vec3(1.0f, 1.0f, 1.0f);
+        position = glm::vec3(0.0f, 0.0f, 0.0f);
+        velocity = glm::vec3(0.0f, 0.0f, 0.0f);
+        movement_speed = 0.0f;
+        isActive = true;
+
+        isStatic = true;
+    }
+    GameObject(Shader& shader, glm::vec3 color)
+    {
+        setShader(shader);
+        this->color = color;
+    }
+    ~GameObject()
+    {
+        glDeleteVertexArrays(1, &vao);
+        glDeleteBuffers(1, &vbo);
+    }
+    void setShader(Shader& shader)
+    {
+        this->shader = &shader;
+    }
+    //가상 함수
+    virtual void drawGameObject(Camera& camera, glm::vec3 lightColor, glm::vec3 lightPos) = 0;
+    void drawMiniGameObject(Camera& camera, glm::vec3 lightColor, glm::vec3 lightPos, glm::vec3 color, glm::vec3 addPos,
+        glm::vec3 mini_scale = glm::vec3(1, 1, 1)
+    )
+    {
+        //fs 셰이더 속성은 drawObject위에
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); //카메라 사이즈
+        //glm::mat4 view = glm::lookAt(camera.Position, glm::vec3(0.0f, 0.0f, 0.0f), glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f)));
+        glm::mat4 view = camera.GetViewMatrix();
+
+
+        //m v p 벡터3
+        shader->setMat4("projection", projection);
+        shader->setMat4("view", view);
+        //debugMat(view);
+
+        shader->setVec3("lightColor", lightColor);
+        shader->setVec3("lightPos", lightPos);
+        shader->setVec3("viewPos", camera.Position);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, position + addPos);
+        model = glm::scale(model, mini_scale);
+        shader->setMat4("model", model);
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6); //삼각형
+    }
+
+    void playerMove(glm::vec3 vec, float deltaTime)
+    {
+        vec = glm::normalize(vec);
+        float velocity = movement_speed * deltaTime;
+
+        move(vec, velocity);
+
+    }
+
+    unsigned int& getVAO()
+    {
+        return vao;
+    }
+    unsigned int& getVBO()
+    {
+        return vbo;
+    }
+    glm::vec3 getPosition()
+    {
+        return position;
+    }
+
+
+    bool isCollisionEnter(GameObject* object)
+    {
+        if (!(this->getIsActive()) || !(object->getIsActive()))
+        {
+            return false;
+        }
+
+        /*
+        if (object->position.y < 2.0f)
+        {
+            std::cout << "bullet_y : " << object->position.y << '\n';
+            std::cout << "ground_y : " << this->position.y << '\n';
+            std::cout << "bullet_scale : " << object->scale.y << '\n';
+            std::cout << "ground_scale: " << this->scale.y << '\n';
+        }
+        */
+
+        // ==이어도 0 ~ 10, 0 ~ 10, 0 ~ 10 즉, 3개가 같아야함 ==> 하나라도 다르면 커트
+        //x비교
+        if (!isInBoundary(this->position.x, object->position.x, this->scale.x, object->scale.x))
+        {
+            return false;
+        }
+
+        if (!isInBoundary(this->position.y, object->position.y, this->scale.y, object->scale.y))
+        {
+            return false;
+        }
+
+        if (!isInBoundary(this->position.z, object->position.z, this->scale.z, object->scale.z))
+        {
+            return false;
+        }
+        //std::cout << object->position.y;
+        return true;
+    }
+
+    //반발력 추가
+    void addRepulsion(float deltaTime)
+    {
+        if (isStatic)
+        {
+            return;
+        }
+        //velocity *= COR; //반발계수
+        if (velocity.y < 0)
+        {
+            velocity *= 0;
+        }
+    }
+
+    void applyPhysics(float deltaTime)
+    {
+        if (isStatic || !isActive)
+        {
+            return;
+        }
+        //a만큼 속력을 추가
+
+        move(velocity, deltaTime);
+        velocity += glm::vec3(0, GRAVITY_ACCELERATION, 0) * deltaTime;
+    }
+
+    glm::vec3 getVelocity()
+    {
+        return velocity;
+    }
+
+    void setVelocity(glm::vec3 velocity)
+    {
+        this->velocity = velocity;
+    }
+
+    bool getIsStatic()
+    {
+        return isStatic;
+    }
+
+    void setIsActive(bool isActive)
+    {
+        this->isActive = isActive;
+    }
+    bool getIsActive()
+    {
+        return isActive;
+    }
+};
+
+#endif
+#pragma once
--- MoreCreatures/src/main.cpp
+++ MoreCreatures/src/main.cpp
@@ -1,14 +1,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <header/camera.h>
+#include <header/shader.h>
+
+#include <GameObject/Ground.h>
+
 #include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+//std_image.h를 이용해서 이미지 열려면 위에 이거 정의해야함
+#include <header/std_image.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+Camera camera(glm::vec3(10.0f, 10.0f, 10.0f));
+glm::vec3 lightPos(2.0f, 5.0f, 2.0f);
+glm::vec3 lightColor(1.0, 1.0, 1.0);
+
+glm::vec3 dir[6] = { glm::vec3(-1.0f,0.0f,1.0f), glm::vec3(1.0f,0,-1.0f), glm::vec3(0,1.0f,0.0f), glm::vec3(0,-1.0f,0.0f), glm::vec3(-1.0f,0.0f,-1.0f), glm::vec3(1.0f,0,1.0f) };
+
+
+//mouse
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+
+void loadTexture(unsigned int& texture, std::string path);
+
 
 int main()
 {
@@ -25,7 +52,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MoreCreatures", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -43,18 +70,31 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+    Shader groundShader("src/vs/ground.vs", "src/fs/ground.fs");
+    Ground* ground = new Ground(groundShader, glm::vec3(1.0f, 1.0f, 1.0f));
+    unsigned int ground_texture;
+
+    loadTexture(ground_texture, "../MoreCreatures/textures/snow.png");
+    ground->setTexture(ground_texture);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         // input
-        // -----
         processInput(window);
 
         // render
-        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //glEnable(GL_DEPTH_TEST);를 추가하면 GL_DEPTH_BUFFER_BIT도 넣어라
+
+        ground->drawGameObject(camera, lightColor, lightPos);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -62,6 +102,7 @@ int main()
         glfwPollEvents();
     }
 
+    delete ground;
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
@@ -74,6 +115,28 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        //player->playerMove(camera.getFrontPlayer(), deltaTime);
+        //camera.move(player->getPosition());
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        //player->playerMove(-camera.getFrontPlayer(), deltaTime);
+        //camera.move(player->getPosition());
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        //player->playerMove(-camera.getRightPlayer(), deltaTime);
+        //camera.move(player->getPosition());
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        //player->playerMove(camera.getRightPlayer(), deltaTime);
+        //camera.move(player->getPosition());
+    }
+
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -83,4 +146,64 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    //camera.ProcessMouseMovement(xoffset, yoffset, player->getPosition());
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+
+//텍스쳐값 로드
+void loadTexture(unsigned int& texture, std::string path)
+{
+    int width, height, nrChannels;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load image, create texture and generate mipmaps
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 }
